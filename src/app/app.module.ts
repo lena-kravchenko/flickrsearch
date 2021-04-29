b9f3663bf804c02081335faf50bdf4277ef56e6d
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-04-29 18:04:44 +0300

    A lot of work should be done yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { NgxPaginationModule } from 'ngx-pagination';
 import { CopyrightsComponent } from './copyrights/copyrights.component';
-import { LoginComponent } from './auth/components/login/login.component';
-import { RegisterComponent } from './auth/components/register/register.component';
+//import { LoginComponent } from './auth/components/login/login.component';
+//import { RegisterComponent } from './auth/components/register/register.component';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchImagesComponent,
     CopyrightsComponent,
-    LoginComponent,
-    RegisterComponent
+//    LoginComponent,
+//    RegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    SharedModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
